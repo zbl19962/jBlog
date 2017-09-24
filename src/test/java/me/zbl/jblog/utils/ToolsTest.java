3627a9c194595460baee3e1997eb0cf5fff6a4d4
James <1146556298@qq.com>
2017-09-24 17:08:27 +0800

    修改多个页面

--- src/test/java/me/zbl/jblog/utils/ToolsTest.java
+++ src/test/java/me/zbl/jblog/utils/ToolsTest.java
@@ -2,6 +2,8 @@ package me.zbl.jblog.utils;
 
 import org.junit.Test;
 
+import java.util.Date;
+
 /**
  * Created by Administrator on 2016/9/3.
  */
@@ -9,5 +11,10 @@ public class ToolsTest {
     @Test
     public void testGetTagList(){
         System.out.println(System.getProperty("user.dir"));
+    }
+
+    @Test
+    public void testTool(){
+        int year = TimeTools.getYear(new Date());
     }
 }
